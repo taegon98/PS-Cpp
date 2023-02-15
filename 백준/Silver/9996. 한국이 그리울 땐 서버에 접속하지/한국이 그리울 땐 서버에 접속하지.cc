7677b14eb0eba3d44c6519b7d9ca1fb7db131757
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-02-15 16:46:20 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -1,52 +1,33 @@
 #include <iostream>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
 int main() {
-	int N, idx = 0, flag;
-	string pattern, file_name, start, end;
+	int N;
+	string pattern, file_name, prefix, suffix;
 
 	cin >> N;
 	cin >> pattern;
 
-	while (idx != pattern.size() and pattern[idx] != '*') {
-		start += pattern[idx++];
-	} idx++;
+	int pos = pattern.find('*');
+	prefix = pattern.substr(0, pos);
+	suffix = pattern.substr(pos + 1, pattern.size());
 
-	while (idx != pattern.size() and pattern[idx] != '*') {
-		end += pattern[idx++];
-	}
-
-	reverse(end.begin(), end.end());
-
-	for (int tc = 0; tc < N; tc++) {
-		flag = 0;
+	for (int i = 0; i < N; i++) {
 		cin >> file_name;
 
-		if (file_name.size() < pattern.size()-1) {
+		if (prefix.size() + suffix.size() > file_name.size()) {
 			cout << "NE" << "\n";
-			continue;
 		}
-		
-		for (int i = 0; i < start.size(); i++) {
-			if (start[i] != file_name[i]) {
-				cout << "NE" << "\n";
-				flag = 1; break;
-			}
+		else {
+			if (prefix == file_name.substr(0, prefix.size()) and
+				suffix == file_name.substr(file_name.size() - suffix.size(), file_name.size()))
+				cout << "DA" << "\n";
+			else cout << "NE" << "\n";
 		}
-
-		if (flag == 0) {
-			for (int i = 0; i < end.size(); i++) {
-				if (end[i] != file_name[file_name.size() - i - 1]) {
-					cout << "NE" << "\n";
-					flag = 1; break;
-				}
-			}
-		}
-		
-		if (flag == 0)
-			cout << "DA" << "\n";
 	}
+	
 	return 0;
 }
