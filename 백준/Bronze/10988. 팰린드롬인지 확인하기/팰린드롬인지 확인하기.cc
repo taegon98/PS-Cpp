22b267a5368b79474e023ab8d6717bd4c2a30e11
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-02-15 14:00:07 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -4,25 +4,13 @@
 using namespace std;
 
 int main() {
-	register int mid, flag = 1;
-	char p1, p2;
-	string s;
+	string s, temp;
 
 	cin >> s;
+	temp = s;
+	reverse(temp.begin(), temp.end());
 
-	if (s.size() % 2 == 1) mid = s.size() / 2;
-	else mid = s.size() / 2 + 1;
-
-	for (int i = 0; i < mid; i++) {
-		p1 = s[i];
-		p2 = s[s.size() - (i + 1)];
-
-		if (p1 != p2) {
-			flag = 0; break;
-		}
-	}
-
-	if (flag == 1) cout << 1;
+	if (s == temp) cout << 1;
 	else cout << 0;
 
 	return 0;
