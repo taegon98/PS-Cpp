b39e7fa1704c31f184af56077e9f31051f3ced22
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-02-15 12:50:29 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int arr[9];
+
+	for (int i = 0; i < 9; i++) cin >> arr[i];
+	
+	sort(arr, arr + 9);
+
+	do {
+		int sum = 0;
+		for (int i = 0; i < 7; i++) sum += arr[i];
+		if (sum == 100) break;
+	} while (next_permutation(arr, arr + 9));
+
+	for (int i = 0; i < 7; i++) cout << arr[i] << "\n";
+
+	return 0;
+}
