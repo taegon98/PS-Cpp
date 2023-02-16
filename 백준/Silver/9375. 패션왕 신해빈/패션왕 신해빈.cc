33247a55b868e949475418c6b1fcbf7ec8f92e97
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-02-16 13:52:40 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+map<string, int> mp;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int T, n;
+	string s;
+
+	cin >> T;
+
+	for (int tc = 1; tc <= T; tc++) {
+		cin >> n;
+		mp.clear();
+		cin.ignore();
+
+		int tot = 1;
+
+		for (int i = 0; i < n; i++) {
+			getline(cin, s);
+			int pos = s.find(' ');
+
+			string tmp = s.substr(pos + 1);
+			if (mp.find(tmp) == mp.end()) {
+				mp.insert(make_pair(tmp, 1));
+			}
+			else mp[tmp]++;
+		}
+
+		for (auto iter = mp.begin(); iter != mp.end(); iter++) {
+			tot *= (iter->second + 1);
+		}
+		cout << tot - 1 << "\n";
+	}
+	
+	return 0;
+}
