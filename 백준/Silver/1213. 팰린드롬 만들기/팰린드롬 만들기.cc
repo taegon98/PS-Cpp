93611bade64f24e41cb715a026d2a206378629c9
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-02-16 15:04:17 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -3,7 +3,6 @@
 using namespace std;
 
 int alpha[26];
-
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
@@ -18,31 +17,27 @@ int main() {
 	for (int i = 0; i < s.size(); i++) {
 		alpha[s[i] - 'A']++;
 	}
-
+	
 	for (int i = 0; i < 26; i++) {
 		if (alpha[i] == 0) continue;
-		if (alpha[i] % 2 == 1) {
+		if (alpha[i] & 1) {
 			ch = i + 'A';
+			s[s.size() / 2] = ch;
 			alpha[i]--;
 			flag++;
 		}
-		if (flag > 1) break;
-	}
-
-	if (flag <= 1) {
-		if (s.size() % 2 == 1) s[s.size() / 2] = ch;
-		
-		for (int i = 0; i < 26; i++) {
-			if (alpha[i] != 0 and alpha[i] % 2 == 0) {
-				for (int j = 0; j < alpha[i] / 2; j++) {
-					s[p1++] = i + 'A';
-					s[p2--] = i + 'A';
-				}
+		if (!(alpha[i] & 1)) {
+			for (int j = 0; j < alpha[i] / 2; j++) {
+				s[p1++] = i + 'A';
+				s[p2--] = i + 'A';
 			}
 		}
+		if (flag > 1) break;
+	}
+	
+	if (flag <= 1) {
 		cout << s << "\n";
 	}
 	else cout << "I'm Sorry Hansoo\n";
-	
 	return 0;
 }
