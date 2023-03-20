c5e75cadf40893d29eea531237c4c4abbe243468
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-03-21 00:21:08 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 8 ms, Memory: 2940 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -4,33 +4,23 @@ using namespace std;
 
 int N, K, x, nx, ret;
 int visited[100004];
-int cnt[100004];
 
 void bfs(int pos) {
 	queue<int> q;
 	q.push(pos);
 	visited[pos] = 1;
-	cnt[pos] = 1;
 
 	while (!q.empty()) {
 		x = q.front(); q.pop();
 		if (x == K) ret++;
 		
-		for (int i = 0; i < 3; i++) {
-			if (i == 0) nx = x - 1;
-			if (i == 1) nx = x + 1;
-			if (i == 2) nx = x * 2;
-
+		for (int nx : {x - 1, x + 1, x * 2}) {
 			if (nx < 0 or nx > 100000) continue;
 			if (!visited[nx]) {
-				cnt[nx]++;
 				q.push(nx);
 				visited[nx] = visited[x] + 1;
 			}
-			else if (visited[nx] >= visited[x] + 1) {
-				cnt[nx]++;
-				q.push(nx);
-			}
+			else if (visited[nx] >= visited[x] + 1) q.push(nx);
 		}
 	}
 }
