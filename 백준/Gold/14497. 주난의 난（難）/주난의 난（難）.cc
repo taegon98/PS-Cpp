e0e38eb9279c7b0d940b6cb1dc1a69ba8b227623
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-03-22 00:12:05 +0900

    [Gold IV] Title: 주난의 난(難), Time: 4 ms, Memory: 2256 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.cc
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.cc
@@ -12,26 +12,35 @@ int dx[4] = { 0,0,-1,1 };
 void bfs(int sy, int sx) {
 	bool visited[304][304];
 	memset(visited, false, sizeof(visited));
-	queue<pair<int, int>> q;
-	q.push({ sy,sx });
+	queue<pair<int, int>> q1;
+	q1.push({ sy,sx });
 	visited[sy][sx] = 1;
 
-	while (!q.empty()) {
-		y = q.front().first; x = q.front().second;
-		q.pop();
-		for (int d = 0; d < 4; d++) {
-			ny = y + dy[d];
-			nx = x + dx[d];
-			if (graph[ny][nx] == '#') {
-				ok = true;
-				return;
-			}
-			if (ny >= 1 and ny <= N and nx >= 1 and nx <= M and !visited[ny][nx]) {
-				visited[ny][nx] = true;
-				if (graph[ny][nx] == '1') graph[ny][nx] = '0';
-				else if (graph[ny][nx] == '0') q.push({ ny,nx });
+	while (1) {
+		ret++;
+		queue<pair<int, int>> q2;
+		while (!q1.empty()) {
+			y = q1.front().first; x = q1.front().second;
+			q1.pop();
+
+			for (int d = 0; d < 4; d++) {
+				ny = y + dy[d];
+				nx = x + dx[d];
+				if (graph[ny][nx] == '#') {
+					ok = true;
+					return;
+				}
+				if (ny >= 1 and ny <= N and nx >= 1 and nx <= M and !visited[ny][nx]) {
+					visited[ny][nx] = true;
+					if (graph[ny][nx] == '1') {
+						q2.push({ ny,nx });
+						graph[ny][nx] = '0';
+					}
+					else if (graph[ny][nx] == '0') q1.push({ ny,nx });
+				}
 			}
 		}
+		q1 = q2;
 	}
 }
 
@@ -45,10 +54,6 @@ int main() {
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= M; j++) cin >> graph[i][j];
 	}
-	while (1) {
-		ret++;
-		bfs(x_1, y_1);
-		if (ok) break;
-	}
+	bfs(x_1, y_1);
 	cout << ret;
 }
