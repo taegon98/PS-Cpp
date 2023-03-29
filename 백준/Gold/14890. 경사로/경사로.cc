7b0d5da639e92a641dd6b539d299512c03f6278f
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-03-29 09:21:07 +0900

    [Gold III] Title: 경사로, Time: 4 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.cc
+++ 백준/Gold/14890. 경사로/경사로.cc
@@ -2,61 +2,32 @@
 using namespace std;
 
 int N, L, ret;
-int graph[104][104];
-int visited1[104][104] = { 0 };
-int visited2[104][104] = { 0 };
+int graph[104][104], graph2[104][104];
 
-int check_row(int r) {
-	int start = graph[r][0];
+int check_row(int arr[][104], int r) {
+	int visited[104][104] = { 0 };
+	int start = arr[r][0];
 	for (int i = 1; i < N; i++) {
-		if (graph[r][i] == start) continue;
-		if (graph[r][i] > start) {
-			start = graph[r][i];
+		if (arr[r][i] == start) continue;
+		if (arr[r][i] > start) {
+			start = arr[r][i];
 			for (int j = 1; j <= L; j++) {
 				int idx = i - j;
-				if (visited1[r][idx]) return 0;
+				if (visited[r][idx]) return 0;
 				if (idx < 0) return 0;
-				if (start - graph[r][idx] != 1) return 0;
-				visited1[r][idx] = 1;
+				if (start - arr[r][idx] != 1) return 0;
+				visited[r][idx] = 1;
 			}
 		}
-		else if (graph[r][i] < start) {
+		else if (arr[r][i] < start) {
 			for (int j = 0; j < L; j++) {
-				if (visited1[r][i]) return 0;
+				if (visited[r][i]) return 0;
 				if (i >= N) return 0;
-				if (start - graph[r][i] != 1) return 0;
-				visited1[r][i++] = 1;
+				if (start - arr[r][i] != 1) return 0;
+				visited[r][i++] = 1;
 			}
 			i--;
-			start = graph[r][i];
-		}
-	}
-	return 1;
-}
-
-int check_col(int c) {
-	int start = graph[0][c];
-	for (int i = 1; i < N; i++) {
-		if (graph[i][c] == start) continue;
-		if (graph[i][c] > start) {
-			start = graph[i][c];
-			for (int j = 1; j <= L; j++) {
-				int idx = i - j;
-				if (visited2[idx][c]) return 0;
-				if (idx < 0) return 0;
-				if (start - graph[idx][c] != 1) return 0;
-				visited2[idx][c] = 1;
-			}
-		}
-		else if (graph[i][c] < start) {
-			for (int j = 0; j < L; j++) {
-				if (visited2[i][c]) return 0;
-				if (i >= N) return 0;
-				if (start - graph[i][c] != 1) return 0;
-				visited2[i++][c] = 1;
-			}
-			i--;
-			start = graph[i][c];
+			start = arr[r][i];
 		}
 	}
 	return 1;
@@ -65,14 +36,14 @@ int check_col(int c) {
 int main() {
 	cin >> N >> L;
 	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++)
+		for (int j = 0; j < N; j++) {
 			cin >> graph[i][j];
+			graph2[j][i] = graph[i][j];
+		}
 	}
 	for (int i = 0; i < N; i++) {
-		ret += check_row(i);
-	}
-	for (int i = 0; i < N; i++) {
-		ret += check_col(i);
+		ret += check_row(graph, i);
+		ret += check_row(graph2, i);
 	}
 	cout << ret;
 }
