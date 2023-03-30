f2e8334e9ba645d010f6c0e04cf5e6b474beec91
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-03-30 17:11:20 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.cc
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -1,16 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int X, l = 64, cnt = 0, ret = 0, size = 0;
+	int X, ret = 0;
 	cin >> X;
-	while (1) {
-		if (size == X or l == X) break;
-		l /= 2;
-		if (size + l <= X) {
-			ret++;
-			size += l;
-		}
+	while (X) {
+		if (X % 2 == 1) ret++;
+		X /= 2;
 	}
-	if (ret == 0) cout << 1;
-	else cout << ret;
+	cout << ret;
 }
