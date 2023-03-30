df417e961c773de1496b4260c4e7a8c09b3909ee
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-03-30 09:40:53 +0900

    [Platinum V] Title: 샤워실 바닥 깔기 (Large), Time: 8 ms, Memory: 2284 KB -BaekjoonHub

---
+++ 백준/Platinum/14601. 샤워실 바닥 깔기 （Large）/샤워실 바닥 깔기 （Large）.cc
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int K, x, y, length, num = 1;
+int graph[140][140];
+
+int check(int r, int c, int k) {
+	for (int i = r; i <= r + pow(2, k) - 1;  i++) {
+		for (int j = c; j <= c + pow(2, k) - 1; j++) {
+			if (graph[i][j] != 0) {
+				if (i < r + pow(2, k) / 2) {
+					if (j < c + pow(2, k) / 2) return 2;
+					else return 1;
+				}
+				else {
+					if (j < c + pow(2, k) / 2) return 3;
+					else return 4;
+				}
+			}
+		}
+	}
+}
+
+void coloring(int q, int s, int y, int x) {
+	y--; x--;
+	if (q == 1) {
+		graph[y + s][x + s] = num;
+		graph[y + s + 1][x + s] = num;
+		graph[y + s + 1][x + s+ 1] = num;
+	}
+	else if (q == 2) {
+		graph[y + s][x + s + 1] = num;
+		graph[y + s + 1][x + s] = num;
+		graph[y + s + 1][x + s + 1] = num;
+	}
+	else if (q == 3) {
+		graph[y + s][x + s] = num;
+		graph[y + s][x + s + 1] = num;
+		graph[y + s + 1][x + s + 1] = num;
+	}
+	else if (q == 4) {
+		graph[y + s][x + s] = num;
+		graph[y + s][x + s + 1] = num;
+		graph[y + s + 1][x + s] = num;
+	}
+	num++;
+}
+
+void go(int sy, int sx, int s) {
+	if (s == 0) return;
+
+	int n = check(sy, sx, s);
+	int j = pow(2, s) / 2;
+	coloring(n, j, sy, sx);
+	
+	go(sy, sx, s - 1);
+	go(sy, sx + j, s - 1);
+	go(sy + j, sx, s - 1);
+	go(sy + j, sx + j, s - 1);
+}
+
+int main() {
+	cin >> K >> x >> y;
+	int ret = 0;
+	/*while (1) {
+		K /= 2;
+		ret++;
+		if (K == 1) break;
+	}*/
+	
+	y = pow(2, K) - y + 1;
+	graph[y][x] = -1;
+	go(1, 1, K);
+
+	/*for (int i = 1; i <= pow(2, K); i++) {
+		for (int j = 1; j <= pow(2, K); j++) {
+			if (graph[i][j] == 0) {
+				cout << "-1";
+				return 0;
+			}
+		}
+	}*/
+
+	for (int i = 1; i <= pow(2, K); i++) {
+		for (int j = 1; j <= pow(2, K); j++) {
+			/*if (graph[i][j] == -1) cout << 0 << " ";*/
+			if (j == pow(2, K)) {
+				cout << graph[i][j];
+			}
+			else
+				cout << graph[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+	
