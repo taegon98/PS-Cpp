7a256e1f12e4acee7ac99756563398e6861a35f0
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-03-30 17:35:24 +0900

    [Gold III] Title: 성곽, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

--- 백준/Gold/2234. 성곽/성곽.cc
+++ 백준/Gold/2234. 성곽/성곽.cc
@@ -4,14 +4,14 @@ using namespace std;
 struct Element {
 	bool n, s, w, e;
 };
-int N, M, tmp, ret1, ret2 = -987654321, ret3 = -987654321;
+int N, M, tmp, ret1, ret2 = -987654321, ret3 = -987654321, num = 1;
 Element element[54][54];
-int visited[54][54];
+int visited[54][54], coloring[2504];
 int dy[4] = { -1,1,0,0 };
 int dx[4] = { 0,0,-1,1 };
 
 int dfs(int sy, int sx, int depth) {
-	visited[sy][sx] = 1;
+	visited[sy][sx] = num;
 	if (!element[sy][sx].n) {
 		if (!visited[sy - 1][sx]) depth += dfs(sy - 1, sx, 1);
 	}
@@ -29,7 +29,6 @@ int dfs(int sy, int sx, int depth) {
 
 int main() {
 	cin >> N >> M;
-	memset(element, false, sizeof(element));
 	for (int i = 1; i <= M; i++) {
 		for (int j = 1; j <= N; j++) {
 			cin >> tmp;
@@ -42,29 +41,19 @@ int main() {
 	for (int i = 1; i <= M; i++) {
 		for (int j = 1; j <= N; j++) {
 			if (!visited[i][j]) {
-				ret2 = max(dfs(i, j, 1), ret2);
+				coloring[num] = dfs(i, j, 1);
+				ret2 = max(coloring[num++], ret2);
 				ret1++;
 			}
 		}
 	}
+
 	for (int i = 1; i <= M; i++) {
-		for (int j = 1; j < N; j++) {
-			if (element[i][j].e) {
-				memset(visited, false, sizeof(visited));
-				element[i][j].e = false;
-				ret3 = max(ret3, dfs(i, j, 1));
-				element[i][j].e = true;
-			}
-		}
-	}
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j < M; j++) {
-			if (element[j][i].s) {
-				memset(visited, false, sizeof(visited));
-				element[j][i].s = false;
-				ret3 = max(ret3, dfs(j, i, 1));
-				element[j][i].s = true;
-			}
+		for (int j = 1; j <= N; j++) {
+			if (j < N and visited[i][j] != visited[i][j + 1])
+				ret3 = max(ret3, coloring[visited[i][j]] + coloring[visited[i][j + 1]]);
+			if (i < M and visited[i][j] != visited[i + 1][j])
+				ret3 = max(ret3, coloring[visited[i][j]] + coloring[visited[i + 1][j]]);
 		}
 	}
 	cout << ret1 << "\n";
