f1b553ad88dad6b0b494245ee90ebfa18f36502f
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-03-31 09:25:46 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	string s;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		cout << s[0] << s[s.size() - 1] << "\n";
+	}
+}
