9c333389d96c51ad59998a672d5b5129803f9160
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-04-06 15:38:53 +0900

    [Gold III] Title: 순회강연, Time: 4 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.cc
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -1,14 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
 using namespace std;
 int n, p, d, ret;
 vector<pair<int, int>> v;
-bool visited[10004];
-
-bool cmp(pair<int, int>& a, pair<int, int>& b) {
-	return a.second > b.second;
-}
+priority_queue<int, vector<int>, greater<int>> pq;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
@@ -17,18 +14,14 @@ int main() {
 		cin >> p >> d;
 		v.push_back({ d,p });
 	}
-	sort(v.begin(), v.end(), cmp);
+	sort(v.begin(), v.end());
 	for (pair<int, int> p : v) {
-		int tmp = p.first;
-		while (1) {
-			if (!visited[tmp])break;
-			if (tmp < 1)break;
-			tmp--;
-		}
-		if (tmp > 0) {
-			visited[tmp] = true;
-			ret += p.second;
-		}
+		pq.push(p.second);
+		if (pq.size() > p.first) pq.pop();
+	}
+	while (pq.size()) {
+		ret += pq.top();
+		pq.pop();
 	}
 	cout << ret;
 }
