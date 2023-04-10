70814e28cbea0851f7913e47b7015828a6ca404b
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-04-10 14:31:33 +0900

    [Silver II] Title: 스타트와 링크, Time: 116 ms, Memory: 3692 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int N, temp;
+int arr[24][24];
+bool visited[24];
+vector<int> ret;
+
+void cal() {
+	vector<int> v1, v2;
+	int tmp1 = 0, tmp2 = 0;
+	for (int i = 1; i <= N; i++) {
+		if (visited[i]) v1.push_back(i);
+		else v2.push_back(i);
+	}
+
+	for (int i = 0; i < v1.size(); i++) {
+		for (int j = i + 1; j < v1.size(); j++) {
+			tmp1 += (arr[v1[i]][v1[j]] + arr[v1[j]][v1[i]]);
+		}
+	}
+	for (int i = 0; i < v2.size(); i++) {
+		for (int j = i + 1; j < v2.size(); j++) {
+			tmp2 += (arr[v2[i]][v2[j]] + arr[v2[j]][v2[i]]);
+		}
+	}
+	ret.push_back(abs(tmp1 - tmp2));
+}
+
+void go(int depth, int idx) {
+	if (depth == N / 2) {
+		cal(); return;
+	}
+	for (int i = idx; i <= N; i++) {
+		if (!visited[i]) {
+			visited[i] = true;
+			go(depth + 1, i + 1);
+			visited[i] = false;
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			cin >> temp;
+			if (i == j) arr[i][j] = 0;
+			else arr[i][j] = temp;
+		}
+	}
+	go(0, 1);
+	sort(ret.begin(), ret.end());
+	cout << ret[0] << "\n";
+}
