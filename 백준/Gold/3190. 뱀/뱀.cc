118805fbc9425d74aea11c7ea0dd7287ffa2a194
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-04-14 18:05:03 +0900

    [Gold IV] Title: 뱀, Time: 0 ms, Memory: 2148 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.cc
+++ 백준/Gold/3190. 뱀/뱀.cc
@@ -1,26 +1,11 @@
 #include <iostream>
 #include <deque>
 using namespace std;
-int N, K, L, y, x, tmp, sy = 1, sx = 1, idx = 1, dir = 4;
-//상->1, 하->2, 좌->3, 우->4
+int N, K, L, y, x, tmp, sy = 1, sx = 1, idx = 1, dir = 1;
 int arr[104][104], order[10004] = { 0 }, visited[104][104] = { 0 };
 char ch;
 deque<pair<int, int>> dq;
-
-void rotate() {
-	if (order[idx] == 1) {
-		if (dir == 4) dir = 2;
-		else if (dir == 2) dir = 3;
-		else if (dir == 3) dir = 1;
-		else if (dir == 1) dir = 4;
-	}
-	else if (order[idx] == 2) {
-		if (dir == 4) dir = 1;
-		else if (dir == 2) dir = 4;
-		else if (dir == 3) dir = 2;
-		else if (dir == 1) dir = 3;
-	}
-}
+int dy[4] = { -1,0,1,0 }, dx[4] = { 0,1,0,-1 };
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
@@ -33,64 +18,27 @@ int main() {
 	for (int i = 1; i <= L; i++) {
 		cin >> tmp >> ch;
 		if (ch == 'D') order[tmp] = 1;
-		else if (ch == 'L') order[tmp] = 2;
+		else if (ch == 'L') order[tmp] = -1;
 	}
 	dq.push_back({ sy,sx });
 	visited[sy][sx] = 1;
 	while (1) {
-		if (dir == 1) {
-			if (visited[sy - 1][sx]) break;
-			if (arr[sy - 1][sx] != -1) {
-				visited[dq.front().first][dq.front().second] = 0; dq.pop_front();
-				dq.push_back({ sy - 1,sx });
-			}
-			else if (arr[sy - 1][sx] == -1) {
-				dq.push_back({ sy - 1,sx });
-				arr[sy - 1][sx] = 0;
-			}
-			visited[sy - 1][sx] = 1;
-			sy--;
+		int ny = dq.back().first + dy[dir];
+		int nx = dq.back().second + dx[dir];
+		if (visited[ny][nx]) break;
+		if (arr[ny][nx] != -1) {
+			visited[dq.front().first][dq.front().second] = 0; dq.pop_front();
+			dq.push_back({ ny,nx });
 		}
-		if (dir == 2) {
-			if (visited[sy + 1][sx]) break;
-			if (arr[sy + 1][sx] != -1) {
-				visited[dq.front().first][dq.front().second] = 0; dq.pop_front();
-				dq.push_back({ sy + 1,sx });
-			}
-			else if (arr[sy + 1][sx] == -1) {
-				dq.push_back({ sy + 1,sx });
-				arr[sy + 1][sx] = 0;
-			}
-			visited[sy + 1][sx] = 1;
-			sy++;
+		else if (arr[ny][nx] == -1) {
+			dq.push_back({ ny,nx });
+			arr[ny][nx] = 0;
 		}
-		if (dir == 3) {
-			if (visited[sy][sx - 1]) break;
-			if (arr[sy][sx - 1] != -1) {
-				visited[dq.front().first][dq.front().second] = 0; dq.pop_front();
-				dq.push_back({ sy,sx - 1 });
-			}
-			else if (arr[sy][sx - 1] == -1) {
-				dq.push_back({ sy,sx - 1 });
-				arr[sy][sx - 1] = 0;
-			}
-			visited[sy][sx - 1] = 1;
-			sx--;
+		visited[ny][nx] = 1;
+		if (order[idx]) {
+			if (dir == 0 and order[idx] == -1) dir = 3;
+			else dir = (dir + order[idx]) % 4;
 		}
-		if (dir == 4) {
-			if (visited[sy][sx + 1]) break;
-			if (arr[sy][sx + 1] != -1) {
-				visited[dq.front().first][dq.front().second] = 0; dq.pop_front();
-				dq.push_back({ sy,sx + 1 });
-			}
-			else if (arr[sy][sx + 1] == -1) {
-				dq.push_back({ sy,sx + 1 });
-				arr[sy][sx + 1] = 0;
-			}
-			visited[sy][sx + 1] = 1;
-			sx++;
-		}
-		if (order[idx]) rotate();
 		if (dq.back().first < 1 || dq.back().second < 1 || dq.back().first > N || dq.back().second > N) break;
 		idx++;
 	}
