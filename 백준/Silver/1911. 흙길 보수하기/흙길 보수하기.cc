674d5a02aed92aec31ebc35b092814a0baad642c
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-04-17 11:14:16 +0900

    [Silver I] Title: 흙길 보수하기, Time: 4 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Silver/1911. 흙길 보수하기/흙길 보수하기.cc
+++ 백준/Silver/1911. 흙길 보수하기/흙길 보수하기.cc
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-int N, L, s, e, ret, tmp;
+int N, L, s, e, ret, tmp, idx;
 vector<pair<int, int>> v;
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
@@ -12,10 +12,19 @@ int main() {
 		v.push_back({ s,e });
 	}
 	sort(v.begin(), v.end());
-
 	for (pair<int, int> p : v) {
-		if (tmp < p.first) tmp = p.first;
-		while (tmp < p.second) ret++, tmp += L;
+		if (idx >= p.second) continue;
+		if (idx < p.first) {
+			tmp = (p.second - p.first) / L;
+			if ((p.second - p.first) % L != 0) tmp += 1;
+			idx = p.first + tmp * L;
+		}
+		else {
+			tmp = (p.second - idx) / L;
+			if ((p.second - idx) % L != 0)tmp += 1;
+			idx = idx + tmp * L;
+		}
+		ret += tmp;
 	}
 	cout << ret;
 }
