85584aa560e0b13364dc9d39425a748a18463a57
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-04-19 09:59:23 +0900

    [Gold V] Title: 선 긋기, Time: 396 ms, Memory: 26728 KB -BaekjoonHub

---
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+ll N, tmp1, tmp2, s, e, ret;
+vector<pair<ll, ll>> v;
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> tmp1 >> tmp2;
+		v.push_back({ tmp1,tmp2 });
+	}
+	sort(v.begin(), v.end());
+	s = v[0].first; e = v[0].second;
+	for (int i = 1; i < v.size(); i++) {
+		if (v[i].first < e) e = v[i].second > e ? v[i].second : e;
+		else if (v[i].first == e) e = v[i].second;
+		else if (v[i].first > e) {
+			ret += (e - s);
+			s = v[i].first; e = v[i].second;
+		}
+	}
+	ret += (e - s);
+	cout << ret;
+}
