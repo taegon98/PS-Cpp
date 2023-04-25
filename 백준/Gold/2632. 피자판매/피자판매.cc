4d9f4349d2755dbd99de18a7e8e96305adcbd205
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-04-25 20:20:23 +0900

    [Gold II] Title: 피자판매, Time: 228 ms, Memory: 56188 KB -BaekjoonHub

--- 백준/Gold/2632. 피자판매/피자판매.cc
+++ 백준/Gold/2632. 피자판매/피자판매.cc
@@ -2,50 +2,40 @@
 #include <vector>
 #include <unordered_map>
 using namespace std;
-int s, n, m, tmp, ret;
-int arr1[1004], arr2[1004];
-vector<int> v1, v2;
+int s, n, m, ret;
+int arr1[1004], arr2[1004], psum1[2004], psum2[2004];
 unordered_map<int, int> mp1, mp2;
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> s >> n >> m;
-	for (int i = 0; i < n; i++) {
-		cin >> arr1[i]; tmp += arr1[i];
+	for (int i = 1; i <= n; i++) {
+		cin >> arr1[i];
+		psum1[i] = psum1[i - 1] + arr1[i];
 	}
-	v1.push_back(tmp);
-	tmp = 0;
-	for (int i = 0; i < m; i++) {
-		cin >> arr2[i]; tmp += arr2[i];
+	for (int i = n + 1; i <= n * 2; i++) {
+		psum1[i] = psum1[i - 1] + arr1[i - n];
 	}
-	v2.push_back(tmp);
-	tmp = 0;
-	for (int i = 0; i < n; i++) {
-		int tot = 0;
-		int idx = i;
-		for (int j = 1; j < n; j++) {
-			tot += arr1[idx];
-			v1.push_back(tot);
-			idx = (idx + 1) % n;
+	for (int i = 1; i <= m; i++) {
+		cin >> arr2[i];
+		psum2[i] = psum2[i - 1] + arr2[i];
+	}
+	for (int i = m + 1; i <= m * 2; i++) {
+		psum2[i] = psum2[i - 1] + arr2[i - m];
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = i; j <= n + i - 1; j++) {
+			mp1[psum1[j] - psum1[j - i]]++;
+			if (i == n) break;
 		}
 	}
-	for (int i = 0; i < m; i++) {
-		int tot = 0;
-		int idx = i;
-		for (int j = 1; j < m; j++) {
-			tot += arr2[idx];
-			v2.push_back(tot);
-			idx = (idx + 1) % m;
+	for (int i = 1; i <= m; i++) {
+		for (int j = i; j <= m + i - 1; j++) {
+			mp2[psum2[j] - psum2[j - i]]++;
+			if(i == m) break;
 		}
 	}
-	for (int num : v1) mp1[num]++;
-	for (int num : v2) mp2[num]++;
-	for (pair<int, int> p : mp1) {
-		tmp = s - p.first;
-		if (p.first == s) ret = ret + p.second;
-		else if (mp2[tmp]) ret = ret + (p.second * mp2[tmp]);
-	}
-	for (pair<int, int> p : mp2) {
-		if (p.first == s) ret = ret + p.second;
-	}
+	ret = mp1[s] + mp2[s];
+	for (int i = 1; i < s; i++) ret += (mp1[i] * mp2[s - i]);
 	cout << ret;
 }
