321c5d29e7ab563d8d3349e5704d4095640544f9
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-05-01 11:11:26 +0900

    [Silver IV] Title: 대칭 차집합, Time: 160 ms, Memory: 21428 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -1,33 +1,24 @@
 #include <iostream>
-#include <vector>
-#include <set>
+#include <unordered_map>
 #include <algorithm>
 using namespace std;
 typedef long long ll;
 ll A, B, tmp, ret;
-vector<ll> v1, v2;
+unordered_map<ll, ll> mp;
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> A >> B;
 	for (int i = 0; i < A; i++) {
 		cin >> tmp;
-		v1.push_back(tmp);
+		mp[tmp]++;
 	}
 	for (int i = 0; i < B; i++) {
 		cin >> tmp;
-		v2.push_back(tmp);
+		mp[tmp]++;
 	}
-	set<ll> s1(v1.begin(), v1.end());
-	set<ll> s2(v2.begin(), v2.end());
-	v1.clear(); v2.clear();
-	for (auto num : s1) v1.push_back(num);
-	for (auto num : s2) v2.push_back(num);
 	
-	for (int target : v1) {
-		if (!binary_search(v2.begin(), v2.end(), target)) ret++;
-	}
-	for (int target : v2) {
-		if (!binary_search(v1.begin(), v1.end(), target)) ret++;
+	for (auto num : mp) {
+		if (num.second == 1) ret++;
 	}
 	cout << ret;
 }
