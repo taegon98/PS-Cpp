00d55a1287028a1f68bf8d8a8296a654da9233c9
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-05-01 09:59:42 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<int> A, B;
+int T, N, M, tmp;
+
+int bin(int left, int right) {
+	int cnt = 0;
+	for (int num : A) {
+		int low = left, high = right, mid;
+		int initial_value = (low + high) / 2;
+		while (low <= high) {
+			mid = (low + high) / 2;
+			if (num <= B[mid]) high = mid - 1;
+			else if (num > B[mid]) low = mid + 1;
+		}
+		if (mid < initial_value) cnt += (high + 1);
+		else if (mid >= initial_value) cnt += (high + 1);
+	}
+	return cnt;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> T;
+	for (int tc = 1; tc <= T; tc++) {
+		A.clear(); B.clear();
+		cin >> N >> M;
+		for (int i = 0; i < N; i++) {
+			cin >> tmp;
+			A.push_back(tmp);
+		}
+		for (int i = 0; i < M; i++) {
+			cin >> tmp;
+			B.push_back(tmp);
+		}
+		sort(A.begin(), A.end());
+		sort(B.begin(), B.end());
+
+		cout << bin(0, M - 1) << "\n"; //A기준 B 이분탐색
+	}
+}
