37eb17b2247a512a3da950de150220ff5e94d211
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-05-04 16:42:04 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n, tmp, idx, ret = -987654321, dp[1004];
+vector<int> v, v_ret;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+	
+	for (int i = 0; i < n; i++) {
+		int len = 1;
+		for (int j = 0; j < i; j++) {
+			if (v[j] < v[i]) {
+				len = max(len, dp[j] + 1);
+			}
+		}
+		dp[i] = len;
+		if (ret < dp[i]) {
+			ret = dp[i];
+			idx = i;
+		}
+	}
+	cout << ret << "\n";
+	tmp = --ret;
+	v_ret.push_back(v[idx]);
+	for (int i = idx; i >= 0; i--) {
+		if (dp[i] == tmp) {
+			v_ret.push_back(v[i]);
+			tmp--;
+		}
+	}
+	reverse(v_ret.begin(), v_ret.end());
+	for (auto a : v_ret) cout << a << " ";
+}
