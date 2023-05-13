2052c2d8e624d11a72197a4cc40964c68e7bffdd
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-05-13 14:24:04 +0900

    [Silver II] Title: 안녕, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.cc
+++ 백준/Silver/1535. 안녕/안녕.cc
@@ -1,21 +1,18 @@
 #include <iostream>
 using namespace std;
-int N, arr1[24], arr2[24], dp[24][104], ret = -987654321;
+int N, arr1[24], arr2[24], dp[104], ret = -987654321;
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> N;
 	for (int i = 1; i <= N; i++) cin >> arr1[i];
 	for (int i = 1; i <= N; i++) cin >> arr2[i];
 	for (int i = 1; i <= N; i++) {
-		for (int j = 0; j <= 99; j++) {
-			if (j < arr1[i]) dp[i][j] = dp[i - 1][j];
-			else if (j >= arr1[i]) {
-				int idx = j - arr1[i];
-				int tmp = dp[i - 1][idx] + arr2[i];
-				dp[i][j] = max(tmp, dp[i - 1][j]);
-			}
+		for (int j = 99; j >= arr1[i]; j--) {
+			int idx = j - arr1[i];
+			int tmp = dp[idx] + arr2[i];
+			dp[j] = max(tmp, dp[j]);
 		}
-		ret = max(ret, dp[i][99]);
+		ret = max(ret, dp[99]);
 	}
 	if (ret == -987654321) cout << -1;
 	else cout << ret;
