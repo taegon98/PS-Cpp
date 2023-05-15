c2d14c5ca7afe08cedcc4363a42f4d4b9713b032
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-05-15 23:07:26 +0900

    [Gold V] Title: 1학년, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.cc
+++ 백준/Gold/5557. 1학년/1학년.cc
@@ -2,22 +2,22 @@
 using namespace std;
 typedef long long ll;
 ll N, arr[104], dp[104][104];
-ll go(int n, int depth, int idx) {
+ll go(int n, int idx) {
 	if (n < 0 or n > 20) return 0;
-	if (depth == N - 1) {
+	if (idx == N - 1) {
 		if (n == arr[N - 1]) return 1;
 		else return 0;
 	}
-	if (dp[n][depth]) return dp[n][depth];
+	if (dp[n][idx]) return dp[n][idx];
 	
-	dp[n][depth] += go(n + arr[idx], depth + 1, idx + 1);
-	dp[n][depth] += go(n - arr[idx], depth + 1, idx + 1);
-	return dp[n][depth];
+	dp[n][idx] += go(n + arr[idx], idx + 1);//누적 값, 현재 인덱스(연산 되어야 하는)
+	dp[n][idx] += go(n - arr[idx], idx + 1);
+	return dp[n][idx];
 }
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> N;
 	for (int i = 0; i < N; i++) cin >> arr[i];
-	cout << go(arr[0], 1, 1);
+	cout << go(arr[0], 1);
 }
