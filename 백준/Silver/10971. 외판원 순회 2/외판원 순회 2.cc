89c6220bc2cf16e6ec825196e92e471890442ee3
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-05-19 17:38:23 +0900

    [Silver II] Title: 외판원 순회 2, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.cc
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.cc
@@ -1,9 +1,10 @@
 #include <iostream>
 using namespace std;
 #define INF 987654321
-int N, ret, arr[14][14], dp[14][1 << 14];
+int N, arr[14][14];
 
 int go(int idx, int num, int cur) {
+	int ret = INF;
 	if (idx == N) {
 		if (arr[cur][1] != 0) return arr[cur][1];
 		else return INF;
@@ -11,17 +12,13 @@ int go(int idx, int num, int cur) {
 	for (int i = 0; i < N; i++) {
 		if (num & (1 << i)) continue;
 		if (arr[cur][i + 1] == 0) continue;
-		dp[cur][num] = min(
-			dp[cur][num], go(idx + 1, num | (1 << i), i + 1) + arr[cur][i + 1]);
+		ret = min(ret, go(idx + 1, num | (1 << i), i + 1) + arr[cur][i + 1]);
 	}
-	return dp[cur][num];
+	return ret;
 }
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	for (int i = 0; i < 14; i++) {
-		for (int j = 0; j < (1 << 14); j++) dp[i][j] = INF;
-	}
 	cin >> N;
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++)
