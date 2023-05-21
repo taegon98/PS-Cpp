2e921a46d250e5cda11c2927d6c0d9ac6bb1496a
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-05-21 16:03:18 +0900

    [Gold II] Title: 색종이 붙이기, Time: 4 ms, Memory: 7980 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.cc
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 #define INF 987654321
-int arr[14][14], ret = INF;
+int arr[14][14], dp[14][14][6][6][6][6][6];
 
 bool check(int y, int x, int n) {
 	for (int i = y; i < y + n; i++) {
@@ -18,74 +18,89 @@ bool done() {
 	return true;
 }
 
-void go(int _1, int _2, int _3, int _4, int _5) {
-	if (_1 > 5 or _2 > 5 or _3 > 5 or _4 > 5 or _5 > 5) return;
-	if (done()) {
-		ret = min(ret, _1 + _2 + _3 + _4 + _5);
-		return;
-	}
+int go(int _1, int _2, int _3, int _4, int _5, int y, int x) {
+    if (_1 > 5 or _2 > 5 or _3 > 5 or _4 > 5 or _5 > 5) return INF;
+	if (done()) return 0;
+	if (dp[y][x][_1][_2][_3][_4][_5]) return dp[y][x][_1][_2][_3][_4][_5];
+	else dp[y][x][_1][_2][_3][_4][_5] = INF;
+
 	for (int i = 1; i <= 10; i++) {
 		for (int j = 1; j <= 10; j++) {
 			if (arr[i][j]) {
 				if (check(i, j, 1)) {
-					for (int r = i; r < i + 1; r++) {
-						for (int c = j; c < j + 1; c++)
+					int r, c;
+					for (r = i; r < i + 1; r++) {
+						for (c = j; c < j + 1; c++)
 							arr[r][c] = 0;
 					}
-					go(_1 + 1, _2, _3, _4, _5);
-					for (int r = i; r < i + 1; r++) {
-						for (int c = j; c < j + 1; c++)
+					dp[y][x][_1][_2][_3][_4][_5] = min(
+						dp[y][x][_1][_2][_3][_4][_5], go(_1 + 1, _2, _3, _4, _5, r, c) + 1);
+					for (r = i; r < i + 1; r++) {
+						for (c = j; c < j + 1; c++)
 							arr[r][c] = 1;
 					}
 				}
+				else return dp[y][x][_1][_2][_3][_4][_5];
 				if (check(i, j, 2)) {
-					for (int r = i; r < i + 2; r++) {
-						for (int c = j; c < j + 2; c++)
+					int r, c;
+					for (r = i; r < i + 2; r++) {
+						for (c = j; c < j + 2; c++)
 							arr[r][c] = 0;
 					}
-					go(_1, _2 + 1, _3, _4, _5);
-					for (int r = i; r < i + 2; r++) {
-						for (int c = j; c < j + 2; c++)
+					dp[y][x][_1][_2][_3][_4][_5] = min(
+						dp[y][x][_1][_2][_3][_4][_5], go(_1, _2 + 1, _3, _4, _5, r, c) + 1);
+					for (r = i; r < i + 2; r++) {
+						for (c = j; c < j + 2; c++)
 							arr[r][c] = 1;
 					}
 				}
+				else return dp[y][x][_1][_2][_3][_4][_5];
 				if (check(i, j, 3)) {
-					for (int r = i; r < i + 3; r++) {
-						for (int c = j; c < j + 3; c++)
+					int r, c;
+					for (r = i; r < i + 3; r++) {
+						for (c = j; c < j + 3; c++)
 							arr[r][c] = 0;
 					}
-					go(_1, _2, _3 + 1, _4, _5);
-					for (int r = i; r < i + 3; r++) {
-						for (int c = j; c < j + 3; c++)
+					dp[y][x][_1][_2][_3][_4][_5] = min(
+						dp[y][x][_1][_2][_3][_4][_5], go(_1, _2, _3 + 1, _4, _5, r, c) + 1);
+					for (r = i; r < i + 3; r++) {
+						for (c = j; c < j + 3; c++)
 							arr[r][c] = 1;
 					}
 				}
+				else return dp[y][x][_1][_2][_3][_4][_5];
 				if (check(i, j, 4)) {
-					for (int r = i; r < i + 4; r++) {
-						for (int c = j; c < j + 4; c++)
+					int r, c;
+					for (r = i; r < i + 4; r++) {
+						for (c = j; c < j + 4; c++)
 							arr[r][c] = 0;
 					}
-					go(_1, _2, _3, _4 + 1, _5);
-					for (int r = i; r < i + 4; r++) {
-						for (int c = j; c < j + 4; c++)
+					dp[y][x][_1][_2][_3][_4][_5] = min(
+						dp[y][x][_1][_2][_3][_4][_5], go(_1, _2, _3, _4 + 1, _5, r, c) + 1);
+					for (r = i; r < i + 4; r++) {
+						for (c = j; c < j + 4; c++)
 							arr[r][c] = 1;
 					}
 				}
+				else return dp[y][x][_1][_2][_3][_4][_5];
 				if (check(i, j, 5)) {
-					for (int r = i; r < i + 5; r++) {
-						for (int c = j; c < j + 5; c++)
+					int r, c;
+					for (r = i; r < i + 5; r++) {
+						for (c = j; c < j + 5; c++)
 							arr[r][c] = 0;
 					}
-					go(_1, _2, _3, _4, _5 + 1);
-					for (int r = i; r < i + 5; r++) {
-						for (int c = j; c < j + 5; c++)
+					dp[y][x][_1][_2][_3][_4][_5] = min(
+						dp[y][x][_1][_2][_3][_4][_5], go(_1, _2, _3, _4, _5 + 1, r, c) + 1);
+					for (r = i; r < i + 5; r++) {
+						for (c = j; c < j + 5; c++)
 							arr[r][c] = 1;
 					}
 				}
-				return;
+				else return dp[y][x][_1][_2][_3][_4][_5];
 			}
 		}
 	}
+	return dp[y][x][_1][_2][_3][_4][_5];
 }
 
 int main() {
@@ -93,7 +108,7 @@ int main() {
 	for (int i = 1; i <= 10; i++)
 		for (int j = 1; j <= 10; j++)
 			cin >> arr[i][j];
-	go(0, 0, 0, 0, 0);
+	int ret = go(0, 0, 0, 0, 0, 1, 1);
 	if (ret == INF) cout << -1;
 	else cout << ret;
 }
