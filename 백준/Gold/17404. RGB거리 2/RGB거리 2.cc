66a5fe60c5ed49e13b3e20a4045029888a09625c
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-05-24 10:46:16 +0900

    [Gold IV] Title: RGB거리 2, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 #define INF 987654321
-int N, dp[1004][3][3], arr[1004][3], start;
+int N, dp[1004][3], arr[1004][3], start, ret = INF;
 
 int go(int idx, int color) {
 	if (idx == N) {
@@ -9,19 +10,19 @@ int go(int idx, int color) {
 		else return arr[idx][color];
 	}
 
-	if (dp[idx][start][color]) return dp[idx][start][color];
-	else dp[idx][start][color] = INF;
+	if (dp[idx][color]) return dp[idx][color];
+	else dp[idx][color] = INF;
 
 	if (color != 0)
-		dp[idx][start][color] = min(
-			dp[idx][start][color], go(idx + 1, 0) + arr[idx][color]);
+		dp[idx][color] = min(
+			dp[idx][color], go(idx + 1, 0) + arr[idx][color]);
 	if (color != 1)
-		dp[idx][start][color] = min(
-			dp[idx][start][color], go(idx + 1, 1) + arr[idx][color]);
+		dp[idx][color] = min(
+			dp[idx][color], go(idx + 1, 1) + arr[idx][color]);
 	if (color != 2)
-		dp[idx][start][color] = min(
-			dp[idx][start][color], go(idx + 1, 2) + arr[idx][color]);
-	return dp[idx][start][color];
+		dp[idx][color] = min(
+			dp[idx][color], go(idx + 1, 2) + arr[idx][color]);
+	return dp[idx][color];
 }
 
 int main() {
@@ -30,8 +31,10 @@ int main() {
 	for (int i = 1; i <= N; i++) {
 		cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
 	}
-	start = 0; int ret1 = go(1, 0);
-	start = 1; int ret2 = go(1, 1);
-	start = 2; int ret3 = go(1, 2);
-	cout << min(min(ret1, ret2), ret3);
+	start = 0; ret = min(ret, go(1, 0));
+	memset(dp, 0, sizeof(dp));
+	start = 1; ret = min(ret, go(1, 1));
+	memset(dp, 0, sizeof(dp));
+	start = 2; ret = min(ret, go(1, 2));
+	cout << ret;
 }
