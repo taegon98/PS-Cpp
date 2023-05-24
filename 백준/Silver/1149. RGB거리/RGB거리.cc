28b7b81ffa68df69d14b34d2e98c3632940e02fe
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-05-24 16:07:49 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,24 +1,20 @@
 #include <iostream>
 using namespace std;
 #define INF 987654321
-int N, ret = INF, dp[1004][3], arr[1004][3];
+int N, ret = INF, dp[2][3];
 
-int go(int idx, int color) {
-	if (idx > N) return 0;
-
-	if (dp[idx][color]) return dp[idx][color];
-	else dp[idx][color] = INF;
-
-	if (color != 0) dp[idx][color] = min(dp[idx][color], go(idx + 1, 0) + arr[idx][0]);
-	if (color != 1) dp[idx][color] = min(dp[idx][color], go(idx + 1, 1) + arr[idx][1]);
-	if (color != 2) dp[idx][color] = min(dp[idx][color], go(idx + 1, 2) + arr[idx][2]);
-	return dp[idx][color];
-}
+void update() { for (int i = 0; i < 3; i++) dp[0][i] = dp[1][i]; }
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> N;
-	for (int i = 1; i <= N; i++) cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
-	for (int i = 0; i < 3; i++) ret = min(ret, go(1, i));
-	cout << ret;
+	cin >> dp[0][0] >> dp[0][1] >> dp[0][2];
+	for (int i = 1; i < N; i++) {
+		cin >> dp[1][0] >> dp[1][1] >> dp[1][2];
+		dp[1][0] += min(dp[0][1], dp[0][2]);
+		dp[1][1] += min(dp[0][0], dp[0][2]);
+		dp[1][2] += min(dp[0][0], dp[0][1]);
+		update();
+	}
+	cout << min(min(dp[0][0], dp[0][1]), dp[0][2]);
 }
