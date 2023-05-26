474ace782385c1f8a6b37947d1d77ba1b13dc475
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-05-26 22:37:33 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int n, arr[304], dp[304];
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> n;
+	for (int i = 1; i <= n; i++) cin >> arr[i];
+	dp[1] = arr[1];
+	dp[2] = arr[1] + arr[2];
+	dp[3] = max(arr[1] + arr[3], arr[2] + arr[3]);
+	for (int i = 4; i <= n; i++) {
+		dp[i] = max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]);
+	}
+	cout << dp[n];
+}
