0c135a619178192265dd59479e29caf1aa08e616
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-06-05 10:35:22 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+ll N, ret, dp[101][10];
+
+ll go(ll num, ll depth) {
+	if (num < 0 or num > 9) return 0;
+	if (depth == N) return 1;
+
+	if (dp[depth][num]) return dp[depth][num];
+
+	dp[depth][num] += go(num + 1, depth + 1) % 1000000000;
+	dp[depth][num] += go(num - 1, depth + 1) % 1000000000;
+	return dp[depth][num] % 1000000000;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> N;
+	for (int i = 1; i <= 9; i++) ret += go(i, 1) % 1000000000;
+	cout << ret % 1000000000;
+}
