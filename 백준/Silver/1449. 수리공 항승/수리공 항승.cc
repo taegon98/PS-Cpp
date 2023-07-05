2667bef95456d49c7b456529efa8b36d216f399b
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-07-06 01:53:15 +0900

    [Silver III] Title: 수리공 항승, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.cc
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.cc
@@ -13,8 +13,7 @@ int main() {
 	}
 	sort(v.begin(), v.end());
 	for (auto pos : v) {
-		if (pos == 0 and visited[pos] and visited[pos + 1]) continue;
-		else if (pos == 2000 and visited[pos - 1] and visited[pos]) continue;
+		if (pos == 2000 and visited[pos - 1] and visited[pos]) continue;
 		else if (visited[pos - 1] and visited[pos] and visited[pos + 1]) continue;
 		pos--; ret++;
 		for (int j = 0; j < L * 2 + 1; j++) {
