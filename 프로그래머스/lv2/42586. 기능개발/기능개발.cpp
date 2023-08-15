8ac03657d54b3249fc949e9096f82a997f1e8e20
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-08-15 13:34:40 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <deque>
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    deque<pair<int, int>> q;
+    for (int i = 0; i < progresses.size(); i++) {
+        q.push_back({progresses[i], speeds[i]});
+    }
+    
+    while(1) {
+        int cnt = 0;
+        if (!q.size()) break;
+        for (int i = 0; i < q.size(); i++) {
+            q[i].first += q[i].second;
+        }
+        while (q.size()) {
+            if (q.front().first < 100) break;
+            else {
+                q.pop_front();
+                cnt++;
+            }
+        }
+        if (cnt) answer.push_back(cnt);
+    }
+    
+    return answer;
+}
