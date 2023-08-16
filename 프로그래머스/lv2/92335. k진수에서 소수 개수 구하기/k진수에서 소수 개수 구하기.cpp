6088f6b8e4c2ccdcedaf252ba4377d15971efdda
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-08-16 15:05:49 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
@@ -15,80 +15,30 @@ bool is_prime(ll n) {
     return true;
 }
 
-void condition1(vector<int> v) {
-    for (int i = 0; i < v.size(); i++) {
-        if (v[i]) continue;
-        ll temp = 0, digit = 1;
-        for (int j = i + 1; j < v.size(); j++) {
-            if (v[j]) continue;
-            for (int k = j - 1; k > i; k--) {
-                temp += (v[k] * digit);
-                digit *= 10;
-            }
-            if (is_prime(temp)) answer++;
-            break;
-        }
-    }
-}
-
-void condition2(vector<int> v) {
-    ll temp = 0, digit = 1;
-    for (int i = 0; i < v.size(); i++) {
-        if (!v[i]) {
-            for (int j = i - 1; j >= 0; j--) {
-                temp += (v[j] * digit);
-                digit *= 10;
-            }
-            if (is_prime(temp)) answer++;
-            return;
-        }
-    }
-    return;
-}
-
-void condition3(vector<int> v) {
-    ll temp = 0, digit = 1;
-    for (int i = v.size() - 1; i >= 0; i--) {
-        if (!v[i]) {
-            for (int j = v.size() - 1; j > i; j--) {
-                temp += (v[j] * digit);
-                digit *= 10;
-            }
-            if (is_prime(temp)) answer++;
-            return;
-        }
-    }
-    return;
-}
-
-void condition4(vector<int> v) {
-    ll temp = 0, digit = 1;
-    for (int i = v.size() - 1; i >= 0; i--) {
-        temp += (v[i] * digit);
-        digit *= 10;
-    }
-    if (is_prime(temp)) answer++;
-    return;
-}
-
 int solution(int n, int k) {
     vector<int> v;
-    bool flag = true;
     while(n) {
         v.push_back(n % k);
         n /= k;
     }
     reverse(v.begin(), v.end());
-    
-    condition1(v);
-    condition2(v);
-    condition3(v);
-    for (int i = 0; i < v.size(); i++) {
-        if (!v[i]) {
-            flag = false;
-            break;
+    string s = "";
+    for (auto var : v) {
+        if (var == 0) {
+            if (s.size() == 0) continue;
+            else {
+                if (is_prime(stoll(s))) answer++;
+                s = "";
+            }
+        }
+        else {
+            s += to_string(var);
         }
     }
-    if (flag) condition4(v);
+    
+    if (s.size()) {
+        if(is_prime(stoll(s))) answer++;
+    }
+    
     return answer;
 }
