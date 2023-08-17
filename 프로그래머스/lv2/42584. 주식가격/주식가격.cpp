f36bf1243067b487a52cc35b2f46cc2e720a124f
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-08-17 14:18:38 +0900

    [level 2] Title: 주식가격, Time: 14.03 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <deque>
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer(prices.size());
+    deque<pair<int, int>> dq;
+    
+    for (int i = 0; i < prices.size(); i++) {
+        if (!dq.size()) dq.push_back({i, prices[i]});
+        else if (dq.size() and dq.back().second <= prices[i])
+            dq.push_back({i, prices[i]});
+        else {
+            while(dq.size() and dq.back().second > prices[i]) {
+                answer[dq.back().first] = i - dq.back().first;
+                dq.pop_back();
+            }
+            dq.push_back({i, prices[i]});
+        }
+    }
+    
+    int idx = dq.back().first;
+    for (auto var : dq) answer[var.first] = idx - var.first;
+    
+    return answer;
+}
