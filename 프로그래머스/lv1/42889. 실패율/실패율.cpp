637b285a6f1a6d8f98a5cb5cf692d02fe61946f5
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-08-19 21:35:32 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.cpp
+++ 프로그래머스/lv1/42889. 실패율/실패율.cpp
@@ -14,18 +14,18 @@ bool cmp(pair<int, double>& a, pair<int, double>& b) {
 vector<int> solution(int N, vector<int> stages) {
     vector<int> answer;
     vector<pair<int, double>> v;
-    int size = stages.size();
+    double size = stages.size();
     
     for (auto var : stages)
         arr[var]++;
     
     for (int i = 1; i <= N; i++) {
-        if (!arr[i]) v.push_back({i, 0});
-        else {
-            v.push_back({i, (double)arr[i] / (double)size});
-            size -= arr[i];
+        if (!arr[i]) {
+            v.push_back({i, 0});
+            continue;
         }
-       
+        v.push_back({i, arr[i] / size});
+        size -= arr[i];
     }
     sort(v.begin(), v.end(), cmp);
     
