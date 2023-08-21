f1d996c5b823d871654f9a6abeb2de2c49c57ebd
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-08-21 15:25:46 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+map<int, int> mp;
+map<int, int> ans;
+int _rank = 1, cnt, i;
+vector<int> solution(vector<int> lottos, vector<int> win_nums) {
+    vector<int> answer;
+    
+    for (int i = 6; i >= 1; i--) {
+        ans[i] = _rank++;
+    }
+    ans[0] = 6;
+    
+    for (auto var : win_nums) mp[var] = 1;
+    
+    sort(lottos.begin(), lottos.end(), greater());
+    
+    for (i = 0; i < lottos.size(); i++) {
+        if (lottos[i] != 0) {
+            if (mp.find(lottos[i]) != mp.end()) cnt++;
+        }
+        else break;
+    }
+    
+    answer.push_back(ans[cnt + 6 - i]);
+    answer.push_back(ans[cnt]);
+    
+    return answer;
+}
