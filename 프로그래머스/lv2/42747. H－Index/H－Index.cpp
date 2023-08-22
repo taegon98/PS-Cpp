824d7243a95121b561dc80c42a04847596eebc1e
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-08-22 17:21:52 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/lv2/42747. H－Index/H－Index.cpp
@@ -3,23 +3,26 @@
 #include <algorithm>
 using namespace std;
 
-bool check(int n, vector<int> c) {
-    int l = 0, h = 0;
-    for (auto var : c) {
-        if (var >= n) h++;
-        else l++;
-    }
-    if (h >= n and l <= n) return true;
-    else return false;
-}
-
 int solution(vector<int> citations) {
     int answer = 0;
     sort(citations.begin(), citations.end(), greater());
-    for (int i = citations[0]; i > 0; i--) {
-        if (check(i, citations)) {
-            answer = i;
-            break;
+    int high = citations[0], low = 0;
+    while(low <= high) {
+        int mid = (low + high) / 2;
+        int l = 0, h = 0;
+        for (auto var : citations) {
+            if (var >= mid) h++;
+            else l++;
+        }
+        if (h < mid) {
+            high = mid - 1;
+        }
+        else if (l > mid) {
+            low = mid + 1;
+        }
+        else {
+            answer = mid;
+            low = mid + 1;
         }
     }
     return answer;
