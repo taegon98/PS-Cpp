46c57781ecf28341e34679c960db499dbac3e1aa
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-08-23 16:52:21 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+map<string, int> mp;
+map<string, int> check;
+map<string, int> info;
+vector<int> answer;
+vector<string> v[1001];
+
+vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+    
+    int idx = 0;
+    for (auto var : id_list) {
+        mp[var] = 0;
+        info[var] = idx++;
+    }
+    
+    for (auto var : report) {
+        if (check.find(var) != check.end()) continue;
+        else check[var]++;
+        
+        int index = var.find(" ");
+        string s = var.substr(0, index);
+        var.erase(0, index + 1);
+        
+        mp[var]++;
+        v[info[s]].push_back(var);
+    }
+    
+    for (int i = 0; i < idx; i++) {
+        int cnt = 0;
+        for (int j = 0; j < v[i].size(); j++) {
+            if (mp[v[i][j]] >= k) cnt++;
+        }
+        answer.push_back(cnt);
+    }
+    return answer;
+}
