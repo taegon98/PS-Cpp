f44297d8f605b8c0577fafe84d6c4f5c6e001a59
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-09-04 11:15:29 +0900

    [level 2] Title: 방문 길이, Time: 0.09 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <map>
+#include <iostream>
+using namespace std;
+int arr[12][12], visited[12][12][5], cur_y = 6, cur_x = 6;
+int dy[] = {-1, 1, 0, 0}, dx[] = {0, 0, -1, 1};
+map<char, int> mp;
+
+int solution(string dirs) {
+    int answer = 0;
+    mp['U'] = 1, mp['D'] = 2, mp['L'] = 3, mp['R'] = 4;
+    
+    for (auto var : dirs) {
+        int ny, nx, cur = mp[var];
+        if (cur == 1) {
+            ny = cur_y + dy[0];
+            nx = cur_x + dx[0];
+        }
+        if (cur == 2) {
+            ny = cur_y + dy[1];
+            nx = cur_x + dx[1];
+        }
+        if (cur == 3) {
+            ny = cur_y + dy[2];
+            nx = cur_x + dx[2];
+        }
+        if (cur == 4) {
+            ny = cur_y + dy[3];
+            nx = cur_x + dx[3];
+        }
+        if (ny <= 0 or nx <= 0 or ny > 11 or nx > 11) continue;
+        if (!visited[cur_y][cur_x][cur]) {
+            int temp;
+            if (cur == 1) temp = 2;
+            if (cur == 2) temp = 1;
+            if (cur == 3) temp = 4;
+            if (cur == 4) temp = 3;
+            
+            visited[cur_y][cur_x][cur] = 1;
+            visited[ny][nx][temp] = 1;
+            answer++;
+        }
+        cur_y = ny; cur_x = nx;
+        cout << cur_y <<"," <<cur_x <<"\n";
+    }
+    return answer;
+}
