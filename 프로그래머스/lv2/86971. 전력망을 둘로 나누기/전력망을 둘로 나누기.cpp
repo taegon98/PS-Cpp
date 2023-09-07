e1ab9345c474142306e34651130ae50559f73c52
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-09-07 16:03:24 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.63 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -18,18 +18,15 @@ int solution(int n, vector<vector<int>> wires) {
     int answer = 987654321;
     for (int i = 0; i < wires.size(); i++) {
         vector<int> v[101];
-        int visited[101] = { 0 }, cnt[2] = { 0 }, idx = 0;
+        int visited[101] = { 0 }, cnt = 0, idx = 0;
         for (int j = 0; j < wires.size(); j++) {
             if (i == j) continue;
             v[wires[j][0]].push_back(wires[j][1]);
             v[wires[j][1]].push_back(wires[j][0]);
         }
         
-        for (int i = 1; i <= n; i++) {
-            if (visited[i]) continue;
-            cnt[idx++] = go(i, visited, v);
-        }
-        answer = min(answer, abs(cnt[0] - cnt[1]));
+        cnt = go(1, visited, v);
+        answer = min(answer, abs(cnt - (n - cnt)));
     }
     return answer;
 }
