268a612d9371855f5bae067955d408c7532a0530
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2023-09-19 11:37:09 +0900

    [level 2] Title: 택배상자, Time: 20.10 ms, Memory: 42.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <stack>
+#include <deque>
+using namespace std;
+stack<int> s;
+deque<int> dq;
+int solution(vector<int> order) {
+    int answer = 0;
+    int cur = 0, tot = order.size();
+
+    for (int i = 1; i <= tot; i++) {
+        dq.push_back(i);
+    }
+    
+    while(dq.size()) {
+        int target = order[cur++];
+        
+        for (auto var : dq) {
+            if (target == var) {
+                answer++;
+                dq.pop_front();
+                break;
+            }
+            else {
+                if (s.size() and s.top() == target) {
+                    s.pop();
+                    answer++;
+                    break;
+                }
+                else s.push(var);
+                dq.pop_front();
+            } 
+        }
+    }
+    
+    while(s.size()) {
+        if (s.top() == order[cur++]) {
+            answer++;
+            s.pop();
+        }
+        else break;
+    }
+    
+    return answer;
+}
