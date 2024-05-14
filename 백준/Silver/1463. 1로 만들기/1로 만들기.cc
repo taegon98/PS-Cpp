35f0474abbbdd0e9bb19c102f06d8d3affc66c9d
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2024-05-14 15:16:58 +0900

    [Silver III] Title: 1로 만들기, Time: 20 ms, Memory: 17524 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,20 +1,33 @@
 #include <iostream>
 using namespace std;
-int N, dp[1000004], ret = 987654321;
+#define INF 987654321
+typedef long long ll;
+ll N;
+ll dp[1000001];
 
-int go(int n) {
-	if (n == 1) return 0;
-	if (dp[n] != 987654321) return dp[n];
+ll go(ll n) {
+	if (n == 1) {
+		return 1;
+	}
 
-	if (n % 3 == 0) dp[n] = min(dp[n], go(n / 3) + 1);
-	if (n % 2 == 0) dp[n] = min(dp[n], go(n / 2) + 1);
-	if (n - 1 >= 1) dp[n] = min(dp[n], go(n - 1) + 1);
-	return dp[n];
+	if (dp[n] != INF) {
+		return dp[n] + 1;
+	}
+
+	if (n % 3 == 0) dp[n] = min(dp[n], go(n / 3));
+	if (n % 2 == 0) dp[n] = min(dp[n], go(n / 2));
+	dp[n] = min(dp[n], go(n - 1));
+
+	return dp[n] + 1;
 }
-
 int main() {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	for (int i = 0; i < 1000004; i++) dp[i] = 987654321;
 	cin >> N;
-	cout << go(N) << "\n";
+	if (N == 1) {
+		cout << 0;
+		return 0;
+	}
+	for (int i = 0; i <= 1000000; i++) dp[i] = INF;
+	go(N);
+
+	cout << dp[N];
 }
