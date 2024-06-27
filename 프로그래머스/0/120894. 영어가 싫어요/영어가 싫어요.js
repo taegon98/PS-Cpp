fa91266ce80cdb5078f588b2b7501d1a98b4fb32
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2024-06-27 19:29:44 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    var answer = 0;
+    numbers = numbers.replaceAll("one", "1").replaceAll("two", "2").replaceAll("three", "3").
+    replaceAll("four", "4").replaceAll("five", "5").replaceAll("six", "6").
+    replaceAll("seven", "7").replaceAll("eight", "8").replaceAll("nine", "9").
+    replaceAll("zero", "0");
+    
+    answer = Number(numbers);
+    
+    return answer;
+}
